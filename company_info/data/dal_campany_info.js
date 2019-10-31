78d60cdf386598b3cd63b6483288c9366f5b78f0
Leônidas do Nascimento <leonidas.nascimento@accenture.com>
2019-10-31 11:03:34 -0300

    First commit

---
+++ company_info/data/dal_campany_info.js
@@ -0,0 +1,53 @@
+const admin = require('firebase-admin');
+const db_collection = 'company_info'
+
+let service_account = require('../sosi_gcp_nosql_service_account.json');
+
+module.exports = class {
+    add_company_info(obj, on_success, on_error) {      
+        this.initialize_app();
+
+        let db = admin.firestore()
+        let doc_ref = db.collection(db_collection).doc(obj.cnpj.toString());
+
+        doc_ref
+            .set(obj)
+            .then(data => {
+                on_success(data);
+            })
+            .catch(data => {
+                on_error(data);
+            });
+    }
+
+    get_company_info_cnpj(doc_id, on_success, on_error) {
+        this.initialize_app();
+
+        let db = admin.firestore();
+        db.collection(db_collection).doc(doc_id)
+            .get()
+            .then((doc) => {
+                if (!doc.exists) {
+                    on_error("Documento '" + doc_id + "' does not exist!")
+                } else {
+                    on_success(doc.data())
+                }
+            })
+            .catch((err) => {
+                on_error('Error getting documents => ' + err)
+            });
+    }
+
+    initialize_app() {
+        if (admin.apps.length <= 0) {
+            admin.initializeApp({
+                credential: admin.credential.cert({
+                    clientEmail: service_account.client_email,
+                    privateKey: String(service_account.private_key).replace(/\\n/g, '\n'),
+                    projectId: service_account.project_id
+                }),
+                databaseURL: "https://" + String(service_account.project_id) + ".firebaseio.com"
+            });
+        }
+    }
+}
---
+++ company_info/routes/index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+var HttpStatus = require('http-status-codes');
+var comp_dao = require('../data/dal_campany_info');
+
+/* GET home page. */
+router.get('/cnpj/', function (req, res, next) {
+  if (Object.keys(req.query).length === 0) {
+    res.status(HttpStatus.LENGTH_REQUIRED).send({
+      "message": "CNPJ is required!"
+    });
+  }
+
+  new comp_dao()
+    .get_company_info_cnpj(req.query['cnpj'], function (data) {
+      res.status(HttpStatus.OK).send(data);
+    }, function (data) {
+      res.status(HttpStatus.METHOD_FAILURE).send(data);
+    });
+});
+
+/* POST Company Info */
+router.post('/', function (req, res, next) { 
+  new comp_dao()
+    .add_company_info(req.body, function (data) {
+      res.status(HttpStatus.OK).send(data);
+    }, function (data) {
+      res.status(HttpStatus.METHOD_FAILURE).send(data);
+    });
+});
+
+module.exports = router;
